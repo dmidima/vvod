6f0b534772ebcfa0c40d1c707fd0781e7652f426
dmidima <102069840+dmidima@users.noreply.github.com>
2022-10-18 16:14:10 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace zadanie2
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            int[] maxi;
+
+            Console.Write("Введите количество элементов массива");
+
+            int elementsCount = int.Parse(Console.ReadLine());
+
+            int[] array = new int[elementsCount];
+
+            for (int i = 0; i < array.Length; i++);
+            {
+                Console.Write("Введите элемент массива");
+                array[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (array[j] > maxi[i])
+                        maxi[i] = array[j];
+                }
+            }
+
+
+            Console.Write("Вывод массива");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(maxi[i]);
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
